0780930cb79756920eaa2627b855249c1b4c5e5b
Jociano Perin <jociano.perin@outlook.com>
2024-03-06 06:58:09 -0300

    Ajuste

--- cleaning/cleaning.py
+++ cleaning/cleaning.py
@@ -1,20 +1,27 @@
 import pandas as pd
 
-def load_data(file_path):
+file_path = '/home/jociano/Projects/PromoPredictor-1/dataset/vendasexport.csv'
+
+# Lê apenas a primeira linha para obter os nomes das colunas
+df = pd.read_csv(file_path, nrows=0)
+print(df.columns.tolist())
+
+def load_data(file_path, usecols=None):
     """
-    Load CSV data from a given file path.
+    Load and optionally filter CSV data from a given file path.
     
     Parameters:
     - file_path: str, path to the CSV file.
+    - usecols: list of str, optional, columns to load from the CSV file.
     
     Returns:
-    - DataFrame containing the loaded data.
+    - DataFrame containing the loaded and optionally filtered data.
     """
-    return pd.read_csv(file_path)
+    return pd.read_csv(file_path, usecols=usecols)
 
 def clean_data(vendas_df, vendas_produtos_df):
     """
-    Clean the data by removing sales and products with negative or zero values.
+    Clean the data by removing sales and products with negative or zero values and filtering unnecessary columns.
     
     Parameters:
     - vendas_df: DataFrame, sales data.
@@ -36,9 +43,17 @@ def main():
     vendas_path = '/home/jociano/Projects/PromoPredictor-1/dataset/vendasexport.csv'
     vendas_produtos_path = '/home/jociano/Projects/PromoPredictor-1/dataset/vendasprodutosexport.csv'
     
-    # Load the datasets
-    vendas_df = load_data(vendas_path)
-    vendas_produtos_df = load_data(vendas_produtos_path)
+    # Necessary columns for each dataset
+    necessary_vendas_columns = ['Codigo', 'Data', 'Hora', 'CodigoCliente', 'TotalPedido']
+    necessary_vendas_produtos_columns = [
+        'CodigoVenda', 'CodigoProduto', 'UNVenda', 'Quantidade', 'ValorTabela',
+        'ValorUnitario', 'ValorTotal', 'Desconto', 'CodigoSecao', 'CodigoGrupo',
+        'CodigoSubGrupo', 'CodigoFabricante', 'ValorCusto', 'ValorCustoGerencial', 'PrecoemPromocao'
+    ]
+    
+    # Load the datasets with necessary columns
+    vendas_df = load_data(vendas_path, usecols=necessary_vendas_columns)
+    vendas_produtos_df = load_data(vendas_produtos_path, usecols=necessary_vendas_produtos_columns)
     
     # Clean the datasets
     vendas_df_clean, vendas_produtos_df_clean = clean_data(vendas_df, vendas_produtos_df)
