5eeab2f93207a0c700ff32ef19c71ebf2a8b0841
Jociano Perin <jociano.perin@outlook.com>
2024-03-16 12:20:08 -0300

    .

--- cleaning/cleaning.py
+++ cleaning/cleaning.py
@@ -1,12 +1,6 @@
 import pandas as pd
 
-file_path = '/home/jociano/Projects/PromoPredictor-1/dataset/vendasexport.csv'
-
-# Lê apenas a primeira linha para obter os nomes das colunas
-df = pd.read_csv(file_path, nrows=0)
-print(df.columns.tolist())
-
-def load_data(file_path, usecols=None):
+def load_data(file_path, usecols):
     """
     Load and optionally filter CSV data from a given file path.
     
@@ -17,7 +11,7 @@ def load_data(file_path, usecols=None):
     Returns:
     - DataFrame containing the loaded and optionally filtered data.
     """
-    return pd.read_csv(file_path, usecols=usecols)
+    return pd.read_csv(file_path, usecols=usecols, sep=';', low_memory=False)
 
 def clean_data(vendas_df, vendas_produtos_df):
     """
@@ -40,9 +34,11 @@ def clean_data(vendas_df, vendas_produtos_df):
 
 def main():
     # Paths to the datasets
-    vendas_path = '/home/jociano/Projects/PromoPredictor-1/dataset/vendasexport.csv'
-    vendas_produtos_path = '/home/jociano/Projects/PromoPredictor-1/dataset/vendasprodutosexport.csv'
-    
+    vendas_path = '/home/jociano/Projects/PromoPredictor/dataset/vendasexport.csv'
+    vendas_produtos_path = '/home/jociano/Projects/PromoPredictor/dataset/vendasprodutosexport.csv'
+    # vendas_path = '/home/jociano/Projects/PromoPredictor/datasettest/vendas.csv'
+    # vendas_produtos_path = '/home/jociano/Projects/PromoPredictor/datasettest/vendasprodutos.csv'
+
     # Necessary columns for each dataset
     necessary_vendas_columns = ['Codigo', 'Data', 'Hora', 'CodigoCliente', 'TotalPedido']
     necessary_vendas_produtos_columns = [
@@ -50,17 +46,17 @@ def main():
         'ValorUnitario', 'ValorTotal', 'Desconto', 'CodigoSecao', 'CodigoGrupo',
         'CodigoSubGrupo', 'CodigoFabricante', 'ValorCusto', 'ValorCustoGerencial', 'PrecoemPromocao'
     ]
-    
+
     # Load the datasets with necessary columns
     vendas_df = load_data(vendas_path, usecols=necessary_vendas_columns)
     vendas_produtos_df = load_data(vendas_produtos_path, usecols=necessary_vendas_produtos_columns)
-    
+        
     # Clean the datasets
     vendas_df_clean, vendas_produtos_df_clean = clean_data(vendas_df, vendas_produtos_df)
     
     # Example of what you can do next: print the shape of the cleaned datasets
     print(f"Cleaned Sales Data Shape: {vendas_df_clean.shape}")
     print(f"Cleaned Products Sold Data Shape: {vendas_produtos_df_clean.shape}")
-
+    print(vendas_produtos_df_clean)
 if __name__ == "__main__":
     main()
