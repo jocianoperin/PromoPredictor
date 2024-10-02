9f3e5fc66f1d884e72fb564b829b976009075af0
Jociano Perin <jociano.perin@outlook.com>
2024-10-02 09:29:32 -0300

    Ajuste

--- promopredictor/main.py
+++ promopredictor/main.py
@@ -13,33 +13,37 @@ def clean_and_prepare_database():
     """
     Limpa o banco de dados e prepara as tabelas necessárias.
     """
-    drop_tables()
-    logger.info("Banco de dados limpo com sucesso.")
+    try:
+        drop_tables()
+        logger.info("Banco de dados limpo com sucesso.")
 
-    create_tables()
-    logger.info("Tabelas criadas com sucesso.")
+        create_tables()
+        logger.info("Tabelas criadas com sucesso.")
 
-    insert_data()
-    logger.info("Dados inseridos nas tabelas com sucesso.")
+        insert_data()
+        logger.info("Dados inseridos nas tabelas com sucesso.")
 
-    configure_indexes()
-    logger.info("Índices configurados com sucesso.")
+        configure_indexes()
+        logger.info("Índices configurados com sucesso.")
+    except Exception as e:
+        logger.error(f"Erro ao limpar e preparar o banco de dados: {e}")
 
 def clean_data():
     """
     Remove registros inválidos e realiza a verificação de tipos de dados.
     """
-    remove_invalid_records("vendasexport", [
-        "totalpedido IS NULL OR totalpedido <= 0",
-        "totalcusto IS NULL OR totalcusto <= 0",
-        "data IS NULL",
-        "hora IS NULL"
-    ])
-    remove_invalid_records("vendasprodutosexport", [
-        "valortabela IS NULL OR valortabela <= 0",
-        "valorunitario IS NULL OR valorunitario <= 0",
-        "valorcusto IS NULL OR valorcusto <= 0"
-    ])
+    try:
+        remove_invalid_records("vendasexport", [
+            "totalpedido IS NULL OR totalpedido <= 0",
+            "totalcusto IS NULL OR totalcusto <= 0",
+            "data IS NULL",
+            "hora IS NULL"
+        ])
+        remove_invalid_records("vendasprodutosexport", [
+            "valortabela IS NULL OR valortabela <= 0",
+            "valorunitario IS NULL OR valorunitario <= 0",
+            "valorcusto IS NULL OR valorcusto <= 0"
+        ])
 
         # Verificação de tipos de dados
         column_types = {'valorunitario': 'DECIMAL(10,2)'}
@@ -47,12 +51,12 @@ def clean_data():
         column_types = {'data': 'DATE'}
         check_data_types('vendasexport', column_types)
 
-        # Remoção de duplicatas
+        # Remoção de duplicatas (desativada por enquanto)
         #remove_duplicates("vendasexport")
         #remove_duplicates("vendasprodutosexport")
         #Difícil localizar um padrão de dados duplicados
 
-        # Detecção e remoção de outliers
+        # Detecção e remoção de outliers (desativada por enquanto)
         #detect_and_remove_outliers('vendasexport', ['totalpedido', 'totalcusto'])
         #detect_and_remove_outliers('vendasprodutosexport', ['valortabela', 'valorunitario', 'valorcusto'])
 
@@ -66,7 +70,17 @@ def clean_data():
 
         logger.info("Processo de inicialização do projeto concluído com sucesso.")
     except Exception as e:
-        logger.error(f"Erro durante o processo de inicialização do projeto: {e}")
+        logger.error(f"Erro durante o processo de limpeza de dados: {e}")
+
+def main():
+    """
+    Função principal para iniciar o processamento do projeto.
+    """
+    logger.info("Iniciando o processo de limpeza e preparação do banco de dados.")
+    clean_and_prepare_database()
+
+    logger.info("Iniciando a limpeza e processamento dos dados.")
+    clean_data()
 
 if __name__ == "__main__":
     main()
